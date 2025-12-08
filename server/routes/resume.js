e3422ee2dcb2826c9d385b074e751dffd945f150
Sagar141005 <samsaini5566@gmail.com>
2025-12-09 03:48:47 +0530

    refactor(resumeRoutes): update routes for new Resume schema and AI endpoints

--- server/routes/resume.js
+++ server/routes/resume.js
@@ -1,22 +1,29 @@
 import express from "express";
 import { protectAndVerifyRole } from "../middlewares/authMiddleware.js";
-import { improveResume, improveResumeForJob, uploadResume, getUserResumes, downloadResumeVersion, downloadCoverLetter, generateCoverLetterForJob } from "../controllers/resume.js";
-
+import {
+  createResume,
+  getResumeById,
+  updateResume,
+  getUserResumes,
+  deleteResume,
+  improveText,
+  createCoverLetter,
+} from "../controllers/resume.js";
 
 const router = express.Router();
 
-router.post('/resume/upload', protectAndVerifyRole(['jobseeker']) , uploadResume);
+router.get("/all", protectAndVerifyRole(["jobseeker"]), getUserResumes);
+router.post("/create", protectAndVerifyRole(["jobseeker"]), createResume);
+router.get("/:id", protectAndVerifyRole(["jobseeker"]), getResumeById);
+router.put("/:id", protectAndVerifyRole(["jobseeker"]), updateResume);
+router.delete("/:id", protectAndVerifyRole(["jobseeker"]), deleteResume);
 
-router.post('/resume/improve/:resumeId', protectAndVerifyRole(['jobseeker']), improveResume);
+router.post("/ai/enhance", protectAndVerifyRole(["jobseeker"]), improveText);
 
-router.post('/resume/improve/:resumeId/:jobId', protectAndVerifyRole(['jobseeker']), improveResumeForJob);
-
-router.get('/resume/all', protectAndVerifyRole(['jobseeker']), getUserResumes);
-
-router.post('/cover-letter/:resumeId/:jobId', protectAndVerifyRole(['jobseeker']), generateCoverLetterForJob);
-
-router.get('/resume/download', protectAndVerifyRole(['jobseeker', 'recruiter']), downloadResumeVersion);
-
-router.get('/cover-letter/download', protectAndVerifyRole(['jobseeker', 'recruiter']), downloadCoverLetter);
+router.post(
+  "/ai/cover-letter",
+  protectAndVerifyRole(["jobseeker"]),
+  createCoverLetter
+);
 
 export default router;
